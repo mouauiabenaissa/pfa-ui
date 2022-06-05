657263be63f176b61086f91b0e3654be167ae991
mouauiabenaissa <benaissamouauia7@gmail.com>
2022-06-05 15:42:47 +0100

    update device interfaces

--- src/pages/Dashboard/InterfacesDevice/InterfacesDevice.js
+++ src/pages/Dashboard/InterfacesDevice/InterfacesDevice.js
@@ -8,87 +8,112 @@ import { Button } from "reactstrap"
 import { toast } from 'react-toastify';
 import classes from "./InterfacesDevice.module.css"
 import InterfacesDeviceItem from "../InterfacesDevice/InterfacesDeviceItem/InterfacesDeviceItem"
-import { Spinner } from "reactstrap"
+import { Spinner, Input } from "reactstrap"
+import LoadingComponent from "../../../Components/Loading/LoadingComponent"
 
 
 function InterfacesDevice(props) {
     const [interfaces, setInterfaces] = React.useState([])
     const [showTable, setShowTable] = React.useState(false)
+    const [loadingSearch, setLoadingSearch] = React.useState(false)
     const [loading, setLoading] = React.useState(false)
     const [IP, setIP] = React.useState([])
+    const [devices, setDevices] = React.useState([])
 
     const handleClickButton = () => {
-        setLoading(true)
+        setLoadingSearch(true)
         axios.post("/device/interface/list", IP).then(response => {
             toast.success("success")
             setShowTable(true)
-            setLoading(false)
+            setLoadingSearch(false)
             setInterfaces(response.data.interfaces)
         }).catch(err => {
             toast.error("error occured")
-            setLoading(false)
+            setLoadingSearch(false)
 
         })
     }
-    console.log(IP)
-    return (
-        <CardComponent
-            containerStyles={props.containerStyles}
-            title="Device Interfaces">
-            <Row style={{ display: "flex", justifyContent: "center" }}>
-                <Col md="6">
-                    <TextField
-                        className={classes.TextFieldFirst}
-                        id="device_ip"
-                        label="Device IP"
-                        placeholder='Device IP'
-                        onChange={(e) => setIP({ ...IP, device_ip: e.target.value })}
-                        variant="standard"
-                        style={{ width: "100%" }}
-                    />
-                </Col>
-            </Row>
-            <Row style={{ display: "flex", justifyContent: "center", marginTop: "40px" }} >
-                <Col md="6">
-                    <Button onClick={handleClickButton} style={{ width: "150px" }} outline>
-                        {loading && <Spinner size="sm" className={classes.spinner} style={{ marginRight: "8px" }} />}
-                        Search
-                    </Button></Col>
-            </Row>
-            {console.log(interfaces)}
-            {showTable ?
-                <div style={{ marginTop: "50px" }} >
-                    <Row >
-                        <Col>
-                            <div className={classes.instanceCreation}>
-                                <h5 className={[classes.itemTitle].join(" ")}>
-                                    This is your list of your interfaces
-                                </h5>
-                            </div>
-                        </Col>
-                    </Row>
-                    <Table style={{ overflow: "none" }}>
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Address IP</th>
-                                <th>Mask</th>
-                                <th >Enabled?</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {[...interfaces].map((interfaceItem, index) => (
-                                <InterfacesDeviceItem
-                                    index={index}
-                                    item={interfaceItem}
-                                    key={index}
-                                />
-                            ))}
-                        </tbody>
-                    </Table>
-                </div> : null}
 
-        </CardComponent>
+    React.useEffect(() => {
+        setLoading(true)
+        axios.get(`/device/list`)
+            .then(res => {
+                setDevices(res.data.devices)
+                setLoading(false)
+            }).catch(err => {
+                setLoading(false)
+                toast.error("error occured")
+            })
+
+
+    }, [])
+    console.log(IP)
+    console.log(devices)
+    return (
+
+        loading ? <LoadingComponent /> :
+            <CardComponent containerStyles={props.containerStyles} title="Device Interfaces" >
+                <Row style={{ display: "flex", justifyContent: "center" }}>
+                    <Col md="6">
+                        <Input
+                            bsSize="lg"
+                            className="mb-3"
+                            type="select"
+                            onChange={(e) => setIP({ ...IP, device_ip: e.target.value })}
+
+                        >
+                            {[...devices].map((device, index) => (
+                                <option key={index}>
+                                    {device.ip}
+                                </option>
+                            ))}
+                        </Input>
+                    </Col>
+                </Row>
+                <Row style={{ display: "flex", justifyContent: "center", marginTop: "40px" }} >
+                    <Col md="6">
+                        <Button onClick={handleClickButton} style={{ width: "150px" }} outline>
+                            {loadingSearch && <Spinner size="sm" className={classes.spinner} style={{ marginRight: "8px" }} />}
+                            Search
+                        </Button></Col>
+                </Row>
+                {console.log(interfaces)}
+                {
+                    showTable ?
+                        <div style={{ marginTop: "50px" }} >
+                            <Row >
+                                <Col>
+                                    <div className={classes.instanceCreation}>
+                                        <h5 className={[classes.itemTitle].join(" ")}>
+                                            This is your list of your interfaces
+                                        </h5>
+                                    </div>
+                                </Col>
+                            </Row>
+                            <Table style={{ overflow: "none" }}>
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Address IP</th>
+                                        <th>Mask</th>
+                                        <th >Enabled?</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {[...interfaces].map((interfaceItem, index) => (
+                                        <InterfacesDeviceItem
+                                            index={index}
+                                            item={interfaceItem}
+                                            key={index}
+                                        />
+                                    ))}
+                                </tbody>
+                            </Table>
+                        </div> : null
+                }
+
+            </CardComponent >
+
     )
 }
 
